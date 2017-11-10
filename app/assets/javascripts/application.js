940b4d176aab6d172326172ef5a24f55138ec8fc
Jeff OConnell <jeffreytoconnell@gmail.com>
2017-11-10 04:33:14 +0000

    Added Bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+//= require popper
+//= require tether
+//= require bootstrap-sprockets
+//= require_tree 
+
