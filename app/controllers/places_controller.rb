1d0a7bc937e371b265142b766ae897bc0256ff63
Jeff OConnell <jeffreytoconnell@gmail.com>
2017-11-16 03:48:24 +0000

    Connecting Users To Places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,5 @@
 class PlacesController < ApplicationController
+    before_action :authenticate_user!, only: [:new, :create]
     def index
         @places = Place.paginate(:page => params[:page], per_page: 3)
     end
@@ -8,7 +9,7 @@ class PlacesController < ApplicationController
     end
 
     def create 
-        Place.create(place_params)
+        current_user.places.create(place_params)
         redirect_to root_path
     end
 
