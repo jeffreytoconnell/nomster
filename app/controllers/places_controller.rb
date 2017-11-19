73ec06ac736e9f9ccab9f6c8997e8c6e65ba2a42
Jeff OConnell <jeffreytoconnell@gmail.com>
2017-11-19 02:19:01 +0000

    Edit User Permissions

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,6 @@
 class PlacesController < ApplicationController
-    before_action :authenticate_user!, only: [:new, :create]
+    before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+   
     def index
         @places = Place.paginate(:page => params[:page], per_page: 3)
     end
@@ -19,6 +20,9 @@ class PlacesController < ApplicationController
 
     def edit
         @place = Place.find(params[:id])
+
+        if @place.user != current_user
+            return render text: 'Not Allowed', status: :forbidden
     end
 
     def destroy
@@ -30,6 +34,10 @@ class PlacesController < ApplicationController
 
     def update 
         @place = Place.find(params[:id])
+        if @place.user != current_user
+                return render text: 'Not Allowed', status: :forbidden
+        end
+        
         @place.update_attributes(place_params)
         redirect_to root_path
     end
