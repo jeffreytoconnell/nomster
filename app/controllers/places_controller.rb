a6f67c0b5bc3ec9f5cd1a94e1da67798fbdc33bd
Jeff OConnell <jeffreytoconnell@gmail.com>
2017-11-19 00:09:09 +0000

    Place detail Page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -13,11 +13,17 @@ class PlacesController < ApplicationController
         redirect_to root_path
     end
 
+    def show
+        @place = Place.find(params[:id])
+    end
+
     private
 
     def place_params
         params.require(:place).permit(:name, :description, :address)
     end
+
+    
 
 
 end
