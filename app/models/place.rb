1df6909a0727250042e0942daa53e1a9e8152250
Jeff OConnell <jeffreytoconnell@gmail.com>
2017-11-26 03:25:50 +0000

    Comments in database

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,6 @@
 class Place < ApplicationRecord
     belongs_to :user
+    has_many :comments
     geocoded_by :address
     after_validation :geocode
     validates :name, length: { minimum: 3 }, presence: true
