4d3326a86d064982bc99b4e6fa4e7597a1cc5996
Jeff OConnell <jeffreytoconnell@gmail.com>
2017-12-02 18:55:18 -0800

    Send email on comment

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,10 @@
 class NotificationMailer < ApplicationMailer
     default from: "no-reply@nomsterapp.com"
 
-    def comment_added
-        mail(to: "jeff19713@gmail.com",
-            subject: "A comment has been added to your place")
-    end
+    def comment_added(comment)
+        @place = comment.place
+        @place_owner = @place.user
+        mail(to: @place_owner.email,
+             subject: "A comment has been added to your place")
+      end
 end
