b386e02d7abefe31d4e472e4fa5cbb39cfcbc9ed
Jeff OConnell <jeffreytoconnell@gmail.com>
2017-12-02 21:41:14 -0800

    User Dashboard Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'places#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :places do
     resources :comments, only: :create
+    resources :photos, only: :create
   end
+  resources :users, only: :show
 end
